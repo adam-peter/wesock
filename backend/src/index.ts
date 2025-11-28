ad15ab40c0a6a2e24c781b426cc94b798295d721
Adam Petržela <your.email@example.com>
2025-11-28 21:30:58 +0100

    fix: be ts types

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 
-const PORT = 8080;
+const PORT = 3000;
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.status(200).send({message: 'hello world!'});
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
