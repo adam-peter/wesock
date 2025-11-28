14fa357c5517f5e731abb9dbf17ee464f4ffb27f
Adam Petržela <your.email@example.com>
2025-11-28 22:47:36 +0100

    feat: setup tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    //@ts-expect-error Tailwind uses outdated types of vite 6 - remove this after update
+    tailwindcss()
+  ],
 });
