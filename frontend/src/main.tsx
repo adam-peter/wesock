120c52668d4dbc1f5f85b1eed21dfd5efbcdb68b
Adam Petržela <your.email@example.com>
2025-11-29 15:21:26 +0100

    feat: setup tweakcn styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router';
 import RootRouter from './routes/Router';
+import { ThemeProvider } from './components/theme-provider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <RootRouter />
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <RootRouter />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 );
